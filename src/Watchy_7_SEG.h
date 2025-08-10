7c245a4c7422ea7aabf663331854408ca33ddc33
gggiovanny <giovanny.gonzalez.19@gmail.com>
2025-08-09 18:01:18 -0600

    copies Starfield theme from source

--- src/Watchy_7_SEG.h
+++ src/Watchy_7_SEG.h
@@ -1,7 +1,9 @@
 #ifndef WATCHY_7_SEG_H
 #define WATCHY_7_SEG_H
-
+#include <math.h>
 #include <Watchy.h>
+#include "Dusk2Dawn.h"
+#include "moonPhaser.h"
 #include "Seven_Segment10pt7b.h"
 #include "DSEG7_Classic_Regular_15.h"
 #include "DSEG7_Classic_Bold_25.h"
@@ -15,8 +17,18 @@ class Watchy7SEG : public Watchy{
         void drawTime();
         void drawDate();
         void drawSteps();
-        void drawWeather();
+//        void drawWeather();
+//        void drawEva();
         void drawBattery();
+//        void drawLine();
+        void drawFiel();
+        void drawMoon();
+
+        void drawSun();
+        virtual void handleButtonPress();
+
+
+
 };
 
 #endif
