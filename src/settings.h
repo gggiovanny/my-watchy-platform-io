8d2538ad31095254ea70a6ba5cbe54a001338c21
Giovanny González Baltazar <giovanny@mifiel.com>
2025-08-12 18:22:49 -0600

    puts DOS example watchface

---
+++ src/settings.h
@@ -0,0 +1,43 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+//Weather Settings
+#define CITY_ID "3531406" //Merida Yucatan Mexico https://openweathermap.org/current#cityid
+
+//You can also use LAT,LON for your location instead of CITY_ID, but not both
+//#define LAT "40.7127" //New York City, Looked up on https://www.latlong.net/
+//#define LON "-74.0059"
+
+#ifdef CITY_ID
+    #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id={cityID}&lang={lang}&units={units}&appid={apiKey}" //open weather api using city ID
+#else
+    #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&lang={lang}&units={units}&appid={apiKey}" //open weather api using lat lon
+#endif
+
+#define OPENWEATHERMAP_APIKEY "a753f2f5b968e568fbdab83df8fc39d7" //use your own API key :) https://openweathermap.org/appid
+#define TEMP_UNIT "metric" //metric = Celsius , imperial = Fahrenheit
+#define TEMP_LANG "en"
+#define WEATHER_UPDATE_INTERVAL 30 //must be greater than 5, measured in minutes
+//NTP Settings
+#define NTP_SERVER "pool.ntp.org"
+#define GMT_OFFSET_SEC 3600 * -6 //Merida Yucatan Mexico is UTC -6 CDT, will be overwritten by weather data
+
+watchySettings settings{
+    #ifdef CITY_ID
+        .cityID = CITY_ID,
+    #else
+        .cityID = "",
+        .lat = LAT,
+        .lon = LON,
+    #endif
+    .weatherAPIKey = OPENWEATHERMAP_APIKEY,
+    .weatherURL = OPENWEATHERMAP_URL,
+    .weatherUnit = TEMP_UNIT,
+    .weatherLang = TEMP_LANG,
+    .weatherUpdateInterval = WEATHER_UPDATE_INTERVAL,
+    .ntpServer = NTP_SERVER,
+    .gmtOffset = GMT_OFFSET_SEC,
+    .vibrateOClock = true,
+};
+
+#endif
